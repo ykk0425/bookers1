e3c6ba0b11507133dcb717e8f27357adc5a121ab
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-01-23 09:18:59 +0000

    [Add]shhowページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def create 
     book = Book.new(book_params)
     book.save
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -11,6 +12,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
