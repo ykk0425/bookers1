abbd79c395cad3ad4ed1ec6eb8698d488972cc17
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-01-23 08:19:57 +0000

    [Add]]一覧ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,23 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
+  def create 
+    book = Book.new(book_params)
+    book.save
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+    
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
